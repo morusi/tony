cf367ea3eaf91ec0c6de0c8ead61f750965a4946
tony <test#gmail.com>
2015-01-04 11:00:13 +0800

    new file

---
+++ if.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Filename: if.py
+number = 23
+guess = int(raw_input('Enter an interger :'))
+
+if guess == number:
+    print 'Congratulations,you guessed it .'
+    print "(but you do not win any prizes!)"
+elif guess < number:
+    print 'No,it is a little higher than that'
+else:
+    print 'No,it is a lower than that'
+
+print 'Done'
